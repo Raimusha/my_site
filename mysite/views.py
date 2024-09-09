1961da3e2528250a6e13e17eec08992a21c649ff
Tristin <tjarvis5@uncc.edu>
2024-09-08 23:35:05 -0400

    59:09

--- mysite/views.py
+++ mysite/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
 from django.urls import path
-from django.http import HttpResponse
+rooms = [
+    {'id':1, 'name':'Lets learn python!'},
+    {'id':2, 'name':'Design with me'},
+    {'id':3, 'name':'Frontend developers'}
+]
 def home(request):
-    return render(request, 'home.html')
+    context = {'rooms': rooms}
+    return render(request, 'base/home.html', context)
 def room(request):
     return render(request, 'room.html')
